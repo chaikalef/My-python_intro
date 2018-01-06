8588eda85b8bea43fce4d7e6fc5b18259d624bc5
Sergey Chaika <chaikalef@gmail.com>
2018-01-06 14:20:26 +0300

    Create pyIntroTask1a.py

---
+++ pyFiles/pyIntroTask1a.py
@@ -0,0 +1,13 @@
+@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# 3^2 — three to the second power
+
+base = float(input("Enter base: "))
+power = float(input("Enter power: "))
+
+print("(" + str(base) + ") ^ (" + str(power) + ") = " + str(base ** power))
